6a252a03b125f5265c20bb564f830e23817f9a68
doguscanavci <d.avci28@gmail.com>
2026-07-02 10:00:00 +0300

    fix: fix palindrome check method

--- Week-03-Java-Data-Structures/04-Solid-Principles/src/main/java/org/example/Main.java
+++ Week-03-Java-Data-Structures/04-Solid-Principles/src/main/java/org/example/Main.java
@@ -10,30 +10,14 @@ public class Main {
     }
 
     public static boolean checkForPalindrome(String input) {
-        // Sadece harfleri al, küçük harfe çevir
         String cleaned = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
-
-        // Stack ile palindrom kontrolü
-        Deque<Character> stack = new ArrayDeque<>();
-
-        // Tüm karakterleri stack'e ekle
-        for (char c : cleaned.toCharArray()) {
-            stack.push(c);
-        }
-
-        // Stack'ten çıkararak orijinalle karşılaştır
-        StringBuilder reversed = new StringBuilder();
-        while (!stack.isEmpty()) {
-            reversed.append(stack.pop());
-        }
-
-        return cleaned.equals(reversed.toString());
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+        return cleaned.equals(reversed);
     }
 
     public static String convertDecimalToBinary(int number) {
         if (number == 0) return "0";
 
-        // Stack kullanarak binary çevirimi
         Deque<Integer> stack = new ArrayDeque<>();
 
         while (number > 0) {
